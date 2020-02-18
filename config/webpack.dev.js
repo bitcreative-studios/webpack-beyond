6e588b0324831a0a6b323a21988c392d2256485c
George Bockari <bitwhys@gmail.com>
2020-02-17 18:05:31 -0800

    add babel

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: {
@@ -11,6 +12,34 @@ module.exports = {
     publicPath: "/",
   },
   devServer: {
+    overlay: true,
     contentBase: "dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          { loader: "file-loader", options: { name: "[name].html" } },
+          { loader: "extract-loader" },
+          { loader: "html-loader", options: { attrs: ["img:src"] } },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          { loader: "file-loader", options: { name: "images/[name].[ext]" } },
+        ],
+      },
+    ],
   },
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-alert("hello wds")
+require("./main.css")
+require("./index.html")
+
+// class Person {
+//   state = {
+//     id: Math.random()
+//       .toString(36)
+//       .substr(-8),
+//   }
+// }
+
+var a = async () => {
+  await console.log("Hello from the future!")
+}
